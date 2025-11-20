5c887d1a394f8c88d24ee0729661216422bbd41b
Marco Rehberg <marcorehberg@gmx.net>
2025-11-20 15:22:57 +0100

    Pages setup

---
+++ frontend/src/pages/AddAnimalPage.tsx
@@ -0,0 +1,9 @@
+export default function AddAnimalPage() {
+  return (
+    <>
+      <main className="p-4">
+        <h2 className="mb-2 text-3xl">Tier einstellen Seite</h2>
+      </main>
+    </>
+  );
+}
