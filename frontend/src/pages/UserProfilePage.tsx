c5e2c01fe381c18b514fec16e59f516fde33024e
Marco Rehberg <marcorehberg@gmx.net>
2025-11-24 15:55:30 +0100

    Headline angleichung

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -2,7 +2,10 @@ export default function UserProfilePage() {
   return (
     <>
       <main className="p-4">
-        <h2 className="mb-2 text-3xl">Benutzerprofil Seite</h2>
+        <h2 className="mb-2 text-5xl font-bold pt-5">Benutzerprofil Seite</h2>
+        <p className="my-10 text-xl">
+          Hier kannst Du alle Infos zu deinem Konto
+        </p>
       </main>
     </>
   );
