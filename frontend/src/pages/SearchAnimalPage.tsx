a590324b31fcec4dae38ded0eb2956026791fbb4
Marco Rehberg <marcorehberg@gmx.net>
2025-11-27 09:11:27 +0100

    Tierkacheln

--- frontend/src/pages/SearchAnimalPage.tsx
+++ frontend/src/pages/SearchAnimalPage.tsx
@@ -29,18 +29,54 @@ export default function SearchAnimalPage() {
         <h1 className="mt-10 mb-4">
           Suchseite - Finde Dein neues Haustier - {slug}
         </h1>
-        <section className="grid xl:grid-cols-3 md:grid-cols-2 gap-3 mb-2">
+
+        <section className="grid xl:grid-cols-3 md:grid-cols-2 gap-9 mb-10">
           {animals?.map((animal) => (
-            <article key={animal._id} className="border min-h-[500px] p-2">
-              <h2 className="mb-4">{animal.name}</h2>
+            <article
+              key={animal._id}
+              className="CARD rounded-2xl bg-[#c7c0ca] flex flex-col"
+            >
               <img
                 src={animal.image_url[0]}
-                title=""
-                className="w-full object-cover h-[340px] border mb-4"
-              ></img>
-              <p>Tierart: </p>
-              <p>Rasse: {animal.race}</p>
-              <p>Alter: {animal.age}</p>
+                alt="Tier sucht Zuhause"
+                className="rounded-t-2xl object-cover h-[360px]"
+              />
+
+              <div className="p-5 font-medium">
+                <div className="flex items-center mb-7">
+                  <img src="/" className="mr-2" />
+                  <h3 className="text-1xl font-bold inline">
+                    Ich bin {animal.name}
+                  </h3>
+                </div>
+
+                <div className="mb-7 flex justify-between">
+                  {/*  Rasse ################################  */}
+                  <div className="flex">
+                    <img
+                      src="img/room-icon.svg"
+                      className="bg-white rounded mr-2"
+                    />
+                    <span>Rasse: {animal.race}</span>
+                  </div>
+
+                  {/*  Alter ################################  */}
+                  <div className="flex">
+                    <img
+                      src="img/measure-icon.svg"
+                      className="bg-white rounded mr-2"
+                    />
+                    <span>Alter: {animal.age} Jahre</span>
+                  </div>
+                </div>
+
+                <div className="flex justify-between">
+                  <button className="rounded-md bg-[#2B1B12] text-white px-4 py-2 cursor-pointer">
+                    Details
+                  </button>
+                  <strong>Rubrtik: "TIERART"</strong>
+                </div>
+              </div>
             </article>
           ))}
         </section>
