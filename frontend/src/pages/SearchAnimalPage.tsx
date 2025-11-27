82099b3ffc38f126d9348a13b5af7cc9b4d36217
Frank Steinmetz <frank.steinmetz@steinmetz-it.de>
2025-11-27 13:09:07 +0100

    Suchmaske  begonnen

--- frontend/src/pages/SearchAnimalPage.tsx
+++ frontend/src/pages/SearchAnimalPage.tsx
@@ -6,6 +6,8 @@ import CardAnimal from "../components/CardAnimal";
 
 export default function SearchAnimalPage() {
   const [animals, setAnimals] = useState<Animal[]>();
+  const [categories, setCategories] = useState<Category[]>();
+  const [characteristics, setCharacteristics] = useState<Characteristic[]>();
 
   const { slug } = useParams<{ slug?: string }>();
 
@@ -26,6 +28,43 @@ export default function SearchAnimalPage() {
     fetchAnimals();
   }, []);
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_APP_AUTH_SERVER_URL}/categories`
+        );
+        const data = await res.json();
+        setCategories(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  useEffect(() => {
+    const fetchCharacteristics = async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_APP_AUTH_SERVER_URL}/characteristics`
+        );
+        const data = await res.json();
+        console.log("Daten: ", data);
+        setCharacteristics(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCharacteristics();
+  }, []);
+
+  const formAction = () => {
+    alert("formAction");
+  };
+
   return (
     <>
       <main className="">
@@ -33,11 +72,40 @@ export default function SearchAnimalPage() {
           Wir suchen ein Zuhause {slug && `- Rubrik: ${slug.toUpperCase()}`}
         </h1>
 
+        <section id="Auswahl" className="mb-2">
+          <h3>Test</h3>
+          <form action={formAction}>
+            <fieldset>
+              <div className="w-52">
+                <legend className="fieldset-legend">
+                  Auswahl Characteristic
+                </legend>
+                <label className="label">
+                  <input type="checkbox" className="checkbox" />
+                  C1
+                </label>
+              </div>
+
+              <label htmlFor="Category" className="input w-18 mr-3">
+                Kategorie
+              </label>
+              <select defaultValue="Auswahl Kategorie" className="select">
+                <option disabled={true}>Kategorie</option>
+                {categories?.map((cat) => (
+                  <option>{cat.categoryName}</option>
+                ))}
+              </select>
+
+              <button type="submit" className="btn">
+                Tiere suchen
+              </button>
+            </fieldset>
+          </form>
+        </section>
         <section className="grid xl:grid-cols-3 md:grid-cols-2 gap-9 mb-10">
           {animals?.map((animal) => (
+            <CardAnimal animal={animal}></CardAnimal>
 
-<CardAnimal animal={animal}></CardAnimal>
-            
             // <article
             //   key={animal._id}
             //   className="CARD rounded-2xl bg-[#c7c0ca] text-black flex flex-col"
