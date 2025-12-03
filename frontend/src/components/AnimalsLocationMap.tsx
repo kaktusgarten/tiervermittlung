09165cd7cf9c2f434f319c55da62a0bff9bd80df
Frank Steinmetz <frank.steinmetz@steinmetz-it.de>
2025-12-03 13:44:01 +0100

    Marker png dazu.

--- frontend/src/components/AnimalsLocationMap.tsx
+++ frontend/src/components/AnimalsLocationMap.tsx
@@ -148,6 +148,19 @@ const AnimalsLocationMap = ({ search }: AnimalsLocationMapProps) => {
     []
   );
 
+  const redIcon = useMemo(
+    () =>
+      L.icon({
+        iconUrl: "../img/marker-icons/marker-icon-red.png",
+        iconSize: [82, 66], // size of the icon
+        shadowSize: [50, 64], // size of the shadow
+        iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62], // the same for the shadow
+        popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+      }),
+    []
+  );
+
   // === Marker versetzen, wenn sie gleiche Koordinaten haben ===
 
   // Tiere nach Koordinaten gruppieren
@@ -203,6 +216,7 @@ const AnimalsLocationMap = ({ search }: AnimalsLocationMapProps) => {
 
         {offsetMarkers.map((animal) => (
           <Marker
+            icon={redIcon}
             position={[animal.lat, animal.lng]}
             eventHandlers={{
               click: () => navigate(`/details/${animal._id}`),
